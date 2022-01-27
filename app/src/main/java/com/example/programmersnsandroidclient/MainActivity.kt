84e8071df5137ea630d3c9b4617034b7872e4f7c
fine <32488002+refine-P@users.noreply.github.com>
2022-01-28 01:01:10 +0900

    [add] ナビゲーション ドロワーを追加
    ついでに、ダークモードで破綻しないようにデザインを変更。

--- app/src/main/java/com/example/programmersnsandroidclient/MainActivity.kt
+++ app/src/main/java/com/example/programmersnsandroidclient/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.programmersnsandroidclient
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph, findViewById<DrawerLayout>(R.id.drawer_layout))
+
+        findViewById<Toolbar>(R.id.toolbar).setupWithNavController(navController, appBarConfiguration)
+        findViewById<NavigationView>(R.id.nav_view).setupWithNavController(navController)
     }
 }
